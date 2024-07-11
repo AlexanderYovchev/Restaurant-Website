37d07acdc9184b9588fe257ad4d4b41bccb50fc3
User <a.yovchev@abv.bg>
2024-07-11 10:42:54 +0300

    Added functional SalesService

---
+++ PracticeWebProjects/Services/SalesService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticeWebProjects.Data;
+using PracticeWebProjects.Data.Models;
+
+namespace PracticeWebProjects.Services
+{
+    public class SalesService
+    {
+        private readonly ApplicationDbContext context;
+
+        public SalesService(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task RegisterSale(int dishId)
+        {
+            var sale = new Sale
+            {
+                DishId = dishId,
+                TransactionDate = DateTime.Now,
+            };
+
+            context.Sales.Add(sale);
+            await context.SaveChangesAsync();
+
+        }
+    }
+}
