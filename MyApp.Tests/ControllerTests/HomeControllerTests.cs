2f10c5228e4ee7ea809686b1fdf48ab288e4979d
User <a.yovchev@abv.bg>
2024-10-17 12:19:24 +0300

    Setup overriden from BaseTest class. CreateOrder ActionResult tests added

--- MyApp.Tests/ControllerTests/HomeControllerTests.cs
+++ MyApp.Tests/ControllerTests/HomeControllerTests.cs
@@ -1,6 +1,9 @@
 ﻿
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Moq;
 using MyApp.Tests.PseudoData;
@@ -10,37 +13,24 @@ using PracticeWebProjects.Data;
 using PracticeWebProjects.Data.Models;
 using PracticeWebProjects.Models;
 using PracticeWebProjects.Services;
+using System.Xml.Linq;
 
 namespace MyApp.Tests.ControllerTests
 {
     [TestFixture]
-    public class HomeControllerTests
+    public class HomeControllerTests : ControllerBaseTest<HomeController, ChefService>
     {
-        private HomeController controller;
 
-        private Mock<ILogger<HomeController>> loggerMock;
-        private ApplicationDbContext context;
-        private ChefService chefService;
-
-
-        [SetUp]
-        public void Setup()
+        protected override ChefService CreateService()
         {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                            .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                            .Options;
-
-            loggerMock = new Mock<ILogger<HomeController>>();
-            context = new ApplicationDbContext(options);
-
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-            context.ChangeTracker.Clear();
-
-            InMemoryDbInitializer.Seed(context);
-            chefService = new ChefService(context);
-            controller = new HomeController(loggerMock.Object, context, chefService);
+            return new ChefService(context);
         }
+
+        protected override HomeController CreateController()
+        {
+            return new HomeController(loggerMock.Object, context, service);
+        }
+
 
         [Test]
         public void InMemoryData_FetchedProperly()
@@ -77,5 +67,112 @@ namespace MyApp.Tests.ControllerTests
             Assert.That(viewResult, Is.InstanceOf<ViewResult>());
         }
 
+        //Check IActionResult CreateOrder method
+        [Test]
+        public void CreateOrder_GET_ReturnsViewModelWithCorrectData()
+        {
+            var model = new DishFromViewModel
+            {
+
+                DishChefs = context.Chefs.Select(c => new DishChefsViewModel
+                {
+                    ChefName = c.Name,
+                    ChefId = c.Id,
+
+                }).ToList()
+            };
+
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model, Is.InstanceOf<DishFromViewModel>());
+            Assert.That(model.DishChefs, Is.InstanceOf<ICollection<DishChefsViewModel>>());
+            Assert.That(model.DishChefs, Is.Not.Null);
+            Assert.That(model.DishChefs.Count, Is.GreaterThan(0));
+
+            var result = controller.CreateOrder();
+            var viewResult = result as ViewResult;
+
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(viewResult, Is.InstanceOf<ViewResult>());
+
+            var dishTypes = viewResult.ViewData["DishTypes"] as SelectList;
+            var chefs = viewResult.ViewData["Chefs"] as SelectList;
+            var servingTables = viewResult.ViewData["ServingTables"] as SelectList;
+
+            Assert.That(dishTypes, Is.Not.Null);
+            Assert.That(chefs, Is.Not.Null);
+            Assert.That(servingTables, Is.Not.Null);
+        }
+
+        [Test]
+        public async Task CreateOrder_POST_InvalidDishType_ReturnsViewWithErrors()
+        {
+            var model = new DishFromViewModel
+            {
+                DishTypeId = 999,
+                ServingTableId = 1,
+                SelectedChefIds = new List<int> { 1, 2 }
+            };
+
+            var result = await controller.CreateOrder(model);
+
+            var viewResult = result as ViewResult;
+
+            Assert.That(viewResult, Is.Not.Null);
+
+            Assert.That(controller.ModelState.IsValid, Is.False);
+            Assert.That(controller.ModelState["DishTypeId"].Errors, Has.Count.GreaterThan(0));
+
+            var dishTypes = viewResult.ViewData["DishTypes"] as SelectList;
+            Assert.That(dishTypes, Is.Not.Null);
+
+        }
+
+        [Test]
+        public async Task CreateOrder_POST_InvalidServingTable()
+        {
+            var model = new DishFromViewModel
+            {
+                DishTypeId = 3,
+                ServingTableId = 10,
+                SelectedChefIds = new List<int> { 1 }
+            };
+
+            var result = await controller.CreateOrder(model);
+            var viewResult = result as ViewResult;
+
+            var servingTable = await context.ServingTables.FirstOrDefaultAsync(st => st.Id == model.ServingTableId);
+
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(controller.ModelState.IsValid, Is.False);
+            Assert.That(servingTable, Is.Null);
+            Assert.That(controller.ModelState["ServingTableId"].Errors, Has.Count.GreaterThan(0));
+        }
+
+        [Test]
+        public async Task CreateOrder_POST_ValidModel_RedirectToAwaitingOrders()
+        {
+            
+            var model = new DishFromViewModel
+            {
+                Id = 1,
+                DishTypeId = 1,
+                ServingTableId = 1,
+                SelectedChefIds = new List<int> { 1 },
+                Name = "Test Dish",
+                Cost = 20,
+            };
+
+            var servingTable = context.ServingTables.First(st => st.Id == model.ServingTableId);
+            servingTable.isTaken = true;
+            context.ServingTables.Update(servingTable);
+            context.SaveChanges();
+
+            var result = await controller.CreateOrder(model);
+            var redirectResult = result as RedirectToActionResult;
+
+            Assert.That(controller.ModelState.IsValid, Is.True);
+            Assert.That(redirectResult, Is.Not.Null);
+            Assert.That(redirectResult.ActionName, Is.EqualTo(nameof(HomeController.AwaitingOrders)));
+        }
     }
 }
